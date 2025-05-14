881f80fdbd13d6b6b2199d7f1588b51e36160f17
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-14 09:22:53 -0300

    Aula 17 Concluida

--- index.php
+++ index.php
@@ -182,7 +182,44 @@
             </p>
             <p>Valor do frete: <?= $valor_frete?></p> -->
     <!-- _____________AULA 17 IF/ELSE PARTE 5 - CONDIÇÕES ENCADEADAS_____________ -->
+        <!-- <?php 
+            // 
+            $usuario_possui_cartao_loja = true;
+            $valor_compra = 450;
+            $valor_frete = 150;
+            $recebeu_desconto_frete = true;
 
+            if($usuario_possui_cartao_loja && $valor_compra >= 400){
+                $valor_frete = 0;
+            }else if($usuario_possui_cartao_loja && $valor_compra >= 300){
+                $valor_frete = 10;
+            }else if($usuario_possui_cartao_loja && $valor_compra >= 100){
+                $valor_frete = 25;
+            }else{
+                $recebeu_desconto_frete = false;
+            }
+            ?>
+            <h1>Detalhes do pedido</h1>
+            <p>Possui Cartão da Loja?
+                <?php 
+                    if($usuario_possui_cartao_loja){
+                        echo 'Sim';
+                    }else{
+                        echo 'Não';
+                    }
+                ?>
+            </p>
+            <p>Valor da compra: <?= $valor_compra?></p>
+            <p>Recebeu desconto no frete?
+                <?php 
+                    if($recebeu_desconto_frete){
+                        echo 'Sim';
+                    }else{
+                        echo 'Não';
+                    }
+                ?>
+            </p>
+            <p>Valor do frete: <?= $valor_frete?></p> -->
     <!-- _____________AULA 18 OPERADORES TERNÁRIOS_____________ -->
 
     <!-- _____________AULA 19 SWITCH_____________ -->
