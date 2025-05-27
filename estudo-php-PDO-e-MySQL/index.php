fa3a677348865ab94c66e6b848304ca12575c0cf
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-27 11:45:59 -0300

    Aula pdo e mysql

--- estudo-php-PDO-e-MySQL/index.php
+++ estudo-php-PDO-e-MySQL/index.php
@@ -2,7 +2,41 @@
     $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
     $usuario = 'root';
     $senha = '';
-    $conexao = new PDO($dsn, $usuario, $senha);
 
-    
+    try{
+        $conexao = new PDO($dsn, $usuario, $senha);
+        /* $query ='
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+        ';
+        $retorno = $conexao->exec($query); // retorna 0
+        echo $retorno; */
+        
+        /* $query ='
+            insert into tb_usuarios(
+                nome, email, senha 
+            )values("Lucas Massaroto", "lucasmassarotodev@gmail.com", "123");
+        ';
+        $retorno = $conexao->exec($query);
+        echo $retorno; */
+
+        $query ='
+            select * from tb_usuarios
+        ';
+
+        $stmt = $conexao->query($query);
+        $lista = $stmt->fetchAll();
+
+        print_r($lista);
+        
+    }catch(PDOException $e){
+        echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
+    }
+
+
+
 ?>
