e6d083e731fcb5e445ff298ea0434fcb16cf693f
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-28 09:57:30 -0300

    aula dpo e mysql feito

--- estudo-php-PDO-e-MySQL/index.php
+++ estudo-php-PDO-e-MySQL/index.php
@@ -1,42 +1,72 @@
 <?php 
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+    
+    if(!empty($_POST['usuario']) && !empty($_POST['senha'])){
 
-    try{
-        $conexao = new PDO($dsn, $usuario, $senha);
-        /* $query ='
-            create table if not exists tb_usuarios(
-                id int not null primary key auto_increment,
-                nome varchar(50) not null,
-                email varchar(100) not null,
-                senha varchar(32) not null
-            )
-        ';
-        $retorno = $conexao->exec($query); // retorna 0
-        echo $retorno; */
-        
-        /* $query ='
-            insert into tb_usuarios(
-                nome, email, senha 
-            )values("Lucas Massaroto", "lucasmassarotodev@gmail.com", "123");
-        ';
-        $retorno = $conexao->exec($query);
-        echo $retorno; */
+        $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+        $usuario = 'root';
+        $senha = '';
+    
+        try{
+            $conexao = new PDO($dsn, $usuario, $senha);
+            /* 
+            ==================BLOCO PARA CRIAR TABELA DE USUARIOS==================
+            $query ='
+                create table tb_usuarios(
+                    id int not null primary key auto_increment,
+                    nome varchar(50) not null,
+                    email varchar(100) not null,
+                    senha varchar(32) not null
+                )
+            ';
+            $retorno = $conexao->exec($query); // retorna 0
+            echo $retorno; 
+            
+            */
 
-        $query ='
-            select * from tb_usuarios
-        ';
+            $query = "select * from tb_usuarios where";
+            $query .= " email = :usuario";
+            $query .= " AND senha = :senha";
+            
+            $stmt = $conexao->prepare($query);
+            $stmt->bindValue(':usuario', $_POST['usuario']);
+            $stmt->bindValue(':senha', $_POST['senha']);
+            $stmt->execute();
+            $usuario = $stmt->fetch();
 
-        $stmt = $conexao->query($query);
-        $lista = $stmt->fetchAll();
+            print_r($usuario);
 
-        print_r($lista);
-        
-    }catch(PDOException $e){
-        echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
+            /*
+            ==================BLOCO PARA DEPURAÇÃO==================
+            $stmt = $conexao->query($query);
+            $lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+            foreach($lista_usuarios as $key => $value){
+                echo $value['nome'];
+                echo '<hr>';
+            }
+            foreach($conexao->query($query) as $key => $value){
+                echo $value['nome'];
+                echo '<hr>';
+            } */
+    
+        }catch(PDOException $e){
+            echo 'Erro: '.$e->getCode().' Mensagem: '.$e->getMessage();
+        }
     }
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="usuario" id="usuario" placeholder="Usuário"><br>
+        <input type="password" name="senha" id="senha" placeholder="Senha"><br>
+        <button type="submit">Logar</button>
+    </form>
+</body>
+</html>
